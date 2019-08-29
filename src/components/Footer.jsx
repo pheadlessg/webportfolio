e61ab815920d7458121ea42951bf82d95bdd073f
pheadlessg <seankfisher@gmail.com>
2019-08-29 13:25:18 -0700

    moved social links and resume to footer and refactored

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import '../CSS/Footer.css';
+import resume from '../documents/resume.pdf';
+import github from '../img/github.png';
+import insta from '../img/insta.png';
+import linkedin from '../img/linkedin.png';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <span className="footertext">Made by Sean Fisher 2019</span>
-      <div className="credit">
-        Icons made by{' '}
-        <a
-          href="https://www.flaticon.com/authors/dave-gandy"
-          title="Dave Gandy"
-        >
-          Dave Gandy
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
+      <div className="footercontainer1">
+        <span className="smlinks">
+          <a href="https://github.com/pheadlessg">
+            <img className="sm" src={github} alt="github"></img>
+          </a>
+          <a href="https://www.instagram.com/sfvox/">
+            <img className="sm" src={insta} alt="instagram"></img>
+          </a>
+          <a href="https://www.linkedin.com/in/seankristianfisher/">
+            <img className="sm" src={linkedin} alt="linkedin"></img>
+          </a>
+        </span>
+        <a className="resumelink" href={resume}>
+          Download Resume
         </a>
+      </div>
+      <div className="footercontainer2">
+        <span className="footertext">Made by Sean Fisher 2019</span>
+        <div className="credit">
+          Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/authors/dave-gandy"
+            title="Dave Gandy"
+          >
+            Dave Gandy
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
 import '../CSS/Header.css';
-import resume from '../documents/resume.pdf';
-import github from '../img/github.png';
-import insta from '../img/insta.png';
-import twitter from '../img/twitter.png';
-import linkedin from '../img/linkedin.png';
 import { Link } from '@reach/router';
 
 const Header = () => {
@@ -15,23 +10,6 @@ const Header = () => {
         <span>Sean Fisher</span>
         <span>Full Stack JS Developer</span>
       </Link>
-      <div className="headerRow2">
-        <span className="smlinks">
-          <a href="https://github.com/pheadlessg">
-            <img className="sm" src={github} alt="github"></img>
-          </a>
-          <a href="https://www.instagram.com/sfvox/">
-            <img className="sm" src={insta} alt="instagram"></img>
-          </a>
-          {/* <img className="sm" src={twitter} alt="twitter"></img> */}
-          <a href="https://www.linkedin.com/in/seankristianfisher/">
-            <img className="sm" src={linkedin} alt="linkedin"></img>
-          </a>
-        </span>
-        <a className="resumelink" href={resume}>
-          Download Resume
-        </a>
-      </div>
       <Nav />
     </div>
   );
