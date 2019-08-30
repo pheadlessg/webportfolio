2b02749541a322a20f6803ab96219dc1487cf360
pheadlessg <seankfisher@gmail.com>
2019-08-29 23:23:48 -0700

    working on adding github links to project cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../CSS/Home.css';
+import { Link } from '@reach/router';
 
 const Home = () => {
   return (
     <div>
       <div className="homebackground"></div>
       <div className="homecontainer">
-        <span className="hometext1">
-          Hi. I'm Sean. I build things with code.
-        </span>
+        <Link className="hometext1" to="/projects/">
+          Hi. I'm Sean. I build things on the internet.
+        </Link>
       </div>
     </div>
   );
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Stack from './Stack';
+import github from '../img/github.png';
 
 const ProjectCard = ({ data }) => {
-  const { title, stack, description, img, link } = data;
+  const { title, stack, description, img, link, githublink } = data;
   return (
     <div className="projectcard">
       <a className="pctitle" href={link}>
         <div className="pctitle">{title}</div>
       </a>
+      {/* <a className="githublink" href={githublink}>
+        <img className="githubpic" src={github} alt="github link"></img>
+      </a> */}
       <div className="pcstack">
         {stack.map((element, index) => {
           return <Stack key={index} data={element} />;
