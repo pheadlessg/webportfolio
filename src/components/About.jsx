f7bd9f98b24c3c5839ceff13ae7e794a5a20ee32
pheadlessg <seankfisher@gmail.com>
2019-09-09 00:26:41 -0700

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,8 @@ const About = () => {
       </span>
       <span className="aboutTextContainer">
         <p>
-          I'm a full stack JavaScript developer based in Southern California.
+          I'm a full stack JavaScript developer based in Southern California,
+          recently moved from Manchester, UK.
         </p>
         <p>
           I build responsive and easy to use UIs and websites using React.js, as
@@ -34,12 +35,18 @@ const About = () => {
           Truffle framework.
         </p>
         <p>
-          I also have extensive experience programming and testing RESTful APIs
-          and developing using Test Driven Development.
+          I have extensive experience programming and testing RESTful APIs and
+          developing using Test Driven Development with Mocha, Chai, Jest,
+          Supertest and Cypress. Since I was a kid I've been adept at fixing
+          broken things - admittedly, usually after I figured out some
+          complicated way to break them - so I find the QA process just as
+          enjoyable as the development process.
         </p>
         <p>
           My goals and interests coincide with being able to build projects
           which expose me to new languages, frameworks and coding practices.
+          Basically, I want to build cool things and learn how to build even
+          cooler things off of the back of that process.
         </p>
         <p>
           I'd say that I'm set apart from most developers through my unique
@@ -48,6 +55,23 @@ const About = () => {
           be affected by the small details but also how to communicate my ideas
           to clients or the rest of my team and engage them with how it can
           affect the end result in a positive way.
+        </p>
+        <p>
+          I learned to code through the prestigious{' '}
+          <a href="https://northcoders.com/">Northcoders </a>
+          coding bootcamp, based in Manchester and Leeds in the UK. While
+          providing a great foundation in JavaScript fundamentals, their focus
+          is always on giving students the tools to learn new languages and
+          frameworks to create their own applications. For example, my final
+          project involved learning to code with Solidity and using the Truffle
+          framework to deploy a voting application to the Ethereum blockchain.
+        </p>
+        <p>
+          When I'm not working on a coding project, I'm usually cooking or
+          baking something. I've always been passionate about food and after
+          spending time working as a food & drink business consultant as well as
+          my time working as a chef de partie, I always like to stretch myself
+          by learning to create delicious new things.
         </p>
         <p>
           If you're interested in my work or have any questions, please get in
